8356f4edae9186f2ae8c024f9dd5b371c6a6d997
Arun Kumar <garunkumarg@gmail.com>
2024-06-21 20:26:08 -0400

    initial commit

---
+++ src/main/java/com/ap/sports/SportsPlayersApplication.java
@@ -0,0 +1,13 @@
+package com.ap.sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsPlayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsPlayersApplication.class, args);
+	}
+
+}
